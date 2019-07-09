069f4fb34447fc5489ae66a5e17045e285911dbe
Hadi Fadlallah <H.Fadlallah@hotmail.com>
2019-07-10 01:21:20 +0300

    Add project files.

---
+++ SSISPackageExplorer/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace SSISPackageExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Connections");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Executables");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Event Handlers");
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Parameters");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Variables");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Precedence Constraints");
+            this.label1 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dtsx file path:";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.Location = new System.Drawing.Point(0, 45);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Connections";
+            treeNode1.Text = "Connections";
+            treeNode2.Name = "Executables";
+            treeNode2.Text = "Executables";
+            treeNode3.Name = "Event Handlers";
+            treeNode3.Text = "Event Handlers";
+            treeNode4.Name = "Parameters";
+            treeNode4.Text = "Parameters";
+            treeNode5.Name = "Variables";
+            treeNode5.Text = "Variables";
+            treeNode6.Name = "Precedence";
+            treeNode6.Text = "Precedence Constraints";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3,
+            treeNode4,
+            treeNode5,
+            treeNode6});
+            this.treeView1.Size = new System.Drawing.Size(784, 516);
+            this.treeView1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(89, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(365, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(460, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 45);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ SSISPackageExplorer/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using DtsRuntime = Microsoft.SqlServer.Dts.Runtime;
+using DtsWrapper = Microsoft.SqlServer.Dts.Pipeline.Wrapper;
+using System.Windows.Forms;
+
+
+namespace SSISPackageExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void FillTree(string path)
+        {
+
+            foreach(TreeNode nd in treeView1.Nodes)
+            {
+                nd.Nodes.Clear();
+
+
+            }
+
+            string pkgLocation;
+            DtsRuntime.Package pkg;
+            DtsRuntime.Application app;
+            
+            pkgLocation = path;
+            app = new DtsRuntime.Application();
+            pkg = app.LoadPackage(pkgLocation, null);
+
+            //List Executables (Tasks)
+            foreach (DtsRuntime.Executable tsk in pkg.Executables)
+            {
+
+
+                DtsRuntime.TaskHost TH = (DtsRuntime.TaskHost)tsk;
+                treeView1.Nodes["Executables"].Nodes.Add(TH.Name, TH.Name);
+
+
+                //Data Flow Task components
+                if (TH.InnerObject.ToString() == "System.__ComObject")
+                {
+                    try
+                    {
+
+                        DtsWrapper.MainPipe m = (DtsWrapper.MainPipe)TH.InnerObject;
+
+
+                        DtsWrapper.IDTSComponentMetaDataCollection100 mdc = m.ComponentMetaDataCollection;
+
+
+                        foreach (DtsWrapper.IDTSComponentMetaData100 md in mdc)
+
+
+                        {
+
+                            //MessageBox.Show(TH.Name.ToString() + " - " + md.Name.ToString());
+                            treeView1.Nodes["Executables"].Nodes[TH.Name].Nodes.Add(md.Name, md.Name);
+
+                        }
+
+                    }
+                    catch
+                    {
+
+
+                    }
+
+
+
+                }
+
+
+
+            }
+
+            //Event Handlers
+            foreach (DtsRuntime.DtsEventHandler eh in pkg.EventHandlers)
+            {
+
+                treeView1.Nodes["Event Handlers"].Nodes.Add(eh.Name, eh.Name);
+
+            }
+
+            //Connection Manager
+
+            foreach (DtsRuntime.ConnectionManager CM in pkg.Connections)
+            {
+
+                
+                treeView1.Nodes["Connections"].Nodes.Add(CM.Name, CM.Name);
+
+            }
+
+
+            //Parameters
+            foreach (DtsRuntime.Parameter Param in pkg.Parameters)
+            {
+
+                treeView1.Nodes["Parameters"].Nodes.Add(Param.Name, Param.Name);
+
+
+            }
+
+
+            //Variables
+            foreach (DtsRuntime.Variable Var in pkg.Variables)
+            {
+                
+                treeView1.Nodes["Variables"].Nodes.Add(Var.Name, Var.QualifiedName);
+
+
+            }
+
+            //Precedence Constraints
+            foreach (DtsRuntime.PrecedenceConstraint PC in pkg.PrecedenceConstraints)
+            {
+
+                
+                treeView1.Nodes["Precedence"].Nodes.Add(PC.Name, PC.Name);
+
+            }
+
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.ShowDialog();
+
+                textBox1.Text = ofd.FileName;
+                FillTree(textBox1.Text);           
+            }
+        }
+    }
+}
